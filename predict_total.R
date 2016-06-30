37cd6eb71c4c2d8d92aaa92585e10ae996da680c
jpacker13 <jpacker@westfield.com>
2016-06-30 13:35:18 -0700

    Add files via upload

---
+++ predict_total.R
@@ -0,0 +1,27 @@
+predict_total <- function(object, data.frame){
+  con <- predict(object, data.frame)
+  
+  ACQ <- readline("Are there Bldg Acquisition Costs? (y/n) ")
+  if(ACQ == "n"){
+    ACQ <- 0
+  }
+  else if(ACQ == "y"){
+    ACQ <- as.integer(readline("Enter DPT value: "))
+  }
+  
+  DPT <- readline("Are there Dept Store Contributions? (y/n) ")
+  if(DPT == "n"){
+    DPT <- 0
+  }
+  else if(DPT == "y"){
+    DPT <- as.integer(readline("Enter DPT value: "))
+  }
+  
+  CNT <- con*.01
+  DES <- con*.08
+  DEV <- con*.02
+  PFE <- con*.03
+  
+  return(data.frame(Construction_Cost = con, Grand_Total = con+CNT+DES+DEV+PFE+ACQ+DPT))
+}
+
---
+++ test_file.R
@@ -0,0 +1,24 @@
+###Intial Data Input###
+raw_data = clean_data("file:///C:/Users/jpacker/Documents/Jeremy_DS_stuff/ProjCostPred/dummy_data_f2.csv")
+
+#Print Data as a check#
+print(raw_data)
+
+###Plot Data###
+with(raw_data, plot((SF_Building), CON, xlab="Sqft", ylab="Construction Costs"))
+
+####Regression Analysis####
+model = lm(CON ~ (SF_Building) + dummy_tier, data = raw_data)
+
+###Test Fit of Model###
+summary(model)
+plot(model)
+
+
+#Test Projects
+project1 = project_object()
+print(project1)
+
+###Test Predictions###
+predict(model, project1)
+predict_total(model, project1)
